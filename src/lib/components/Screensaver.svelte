5370c531cb657b7157709ef7362a3c686da3e987
harijsvucens <Harijsvucens@gmail.com>
2026-01-18 21:29:23 +0200

    fix scrsvr

--- src/lib/components/Screensaver.svelte
+++ src/lib/components/Screensaver.svelte
@@ -74,7 +74,7 @@
 
 {#if active && currentImage}
 	<div class="fixed inset-0 z-40 bg-white" transition:fade={{ duration: 1000 }}>
-		<h1 class="fixed top-2 left-2 z-50 mb-8 text-black uppercase mix-blend-difference">
+		<h1 class="fixed top-2 left-2 z-50 mb-8 text-white uppercase mix-blend-difference">
 			UĢIS TRAUMANIS
 		</h1>
 		{#key currentImageIndex}
